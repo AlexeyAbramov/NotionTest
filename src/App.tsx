936fa36d1dad102648758467a5630e42eeda37fd
TheNear <thenearx@gmail.com>
2020-11-12 13:55:52 +0300

    init app with eslint

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">dsafads</header>
+  </div>
+);
+
+export { App };
---
+++ src/types/users.ts
@@ -0,0 +1,88 @@
+export interface UserApiResponse {
+  results: IUserInfo[],
+  info: ApiResponseInfo,
+}
+
+export interface ApiResponseInfo {
+  seed: string,
+  results: number,
+  page: number,
+  version: string,
+}
+
+export interface IUserInfo {
+  gender: string,
+  name: UserName,
+  location: UserLoc,
+  email: string,
+  login: UserLoginData,
+  dob: UserBirthday,
+  registered: UserRegDate,
+  phone: string,
+  cell: string,
+  id: UserID,
+  picture: UserPicture,
+  nat: string,
+}
+
+export interface UserName {
+  title: string;
+  first: string;
+  last: string;
+}
+
+export interface UserLoc {
+  street: UserLocStreet;
+  city: string;
+  state: string;
+  country: string;
+  postcode: number;
+  coordinates: UserLocCoord;
+  timezone: UserLocTimezone;
+}
+
+export interface UserLocStreet {
+  number: number;
+  name: string;
+}
+
+export interface UserLocCoord {
+  latitude: string;
+  longitude: string;
+}
+
+export interface UserLocTimezone {
+  offset: string;
+  description: string;
+}
+
+export interface UserLoginData {
+  uuid: string,
+  username: string,
+  password: string,
+  salt: string,
+  md5: string,
+  sha1: string,
+  sha256: string,
+}
+
+export interface UserBirthday {
+  date: string,
+  age: number,
+}
+
+export interface UserRegDate {
+  date: string,
+  age: number,
+}
+
+export interface UserID {
+  name: string,
+  value: string,
+}
+
+export interface UserPicture {
+  large: string,
+  medium: string,
+  thumbnail: string,
+}
