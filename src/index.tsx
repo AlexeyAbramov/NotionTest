98e9e3b77e94c51f3a3ca082e2427db4572745c4
TheNear <thenearx@gmail.com>
2020-11-20 09:37:05 +0300

    add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import { App } from "./App";
 import { GlobalStyle, theme } from "./GlobalTheme";
+import { store } from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root"),
 );
